6440417ed02613abb85398a6104dcb9ecfece1ae
Benjamin Zifcak <BmZifcak@udel.edu>
2025-10-13 12:52:35 -0400

    Added broad structure for randomized board generation

--- src/board.cpp
+++ src/board.cpp
@@ -1,11 +1,23 @@
 #include "unit.h" 
 #include "player.h" 
 #include "board.h"
+#include <cstdlib> // For rand and srand
+#include <ctime> // For time()
 
 board::board(int s):size(s){
+    srand(static_cast<unsigned>(time(0)));
         grid = new unit*[s*s];
         for(int i=0;i<s*s;i++){
-            grid[i]=nullptr;
+            int randomType = rand() % 4; // what data type (int, char, bool, or float) 1-4 respectively
+            int randomComplex = rand() % 4; // complex or not (0-2 not complex and 3 complex, this makes it weighted so there are fewer complex units)
+                if( randomComplex){
+                    int randomComplexType = rand() % 5; // random complex types (array, DLL, BT, Hash, matrix or someother types)
+                        switch randomComplexType //descriminate to figure out type 
+                             grid[i]=nullptr; // replace with randomly complex unit of randomType. for complex units, iterate through board and set the first null pointer to the adress or something similar
+                } else {
+                    grid[i]=nullptr; // replace with unit of randomType
+                }
+            
         }
 }
 board::~board(){
--- src/player.h
+++ src/player.h
@@ -1,11 +1,19 @@
 #include "unit.h" 
+typedef struct{
+    int r;
+    int g;
+    int b;
+} rgb
+
 class player{
     int x;
     int y;
+    rgb color;
     unit *territory[];
+    int turns;
     public:
     player(int x,int y):x(x),y(y){}
-    addTerritory(unit *u){
+    void addTerritory(unit *u){
         territory.push_back(u);
         u->setOwner(this);
     }
